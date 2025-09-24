d015ce5b3abdc97328bbc9ad1f6fb910f88af2bd
malkan-yn <yogiangga23@gmail.com>
2025-09-24 08:45:03 +0700

    Add files via upload

---
+++ Koneksi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManajemenToko
+{
+    internal class Koneksi
+    {
+        public static SqlConnection GetConnection()
+        {
+            string connectionString = @"Data Source=localhost;Initial Catalog=TokoDB;Integrated Security=True;";
+            return new SqlConnection(connectionString);
+        }
+    }
+}
